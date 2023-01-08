b78e402384becc29492a4fda435e6581cc6d08e4
LisaMLorenz <hej@frau-lorenz.de>
2023-01-08 14:41:35 +0000

    numberals and special characters

--- starter/script.js
+++ starter/script.js
@@ -88,7 +88,16 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+console.log(upperCasedCharacters);
+
 // Function to prompt user for password options
+var passwordLength = 0;
+var passLowerCase = 0;
+var passUpperCase = 0;
+var userPassword = [0];
+
+// Input to choose password length.
+
 var passwordLength = prompt("How many characters long is your password going to be?"); // create an if statement to limit password length 10 - 64 characters
 
 if (passwordLength < 10) {
@@ -96,36 +105,103 @@ if (passwordLength < 10) {
 } else if (passwordLength > 64) {
   prompt("Sorry. Choose a password that is no longer than 64 but has at least 10 characters.")
 }
-else if (passwordLength > 9) {
-  console.log("Password length chosen.");
-}
+
+console.log("Password length chosen: " + passwordLength);
 
 
-const passLowerCase = confirm("Should your password contain lowercase characters?");
+// Alert to choose Upper Case
+
+var passLowerCase = confirm("Should your password contain lowercase characters?");
 
 if (confirm == true) {
-  userPassword.push(lowerCasedCharacters); // then access lowerCasedCharacters array with possibly with "Math.random().toString(36).slice(2, 10)" from StackOverflow (https://stackoverflow.com/questions/1497481/javascript-password-generator)
+
+  function getRandomLower(passwordLength) {
+    var RandomLowerString = []
+    return Math.floor(Math.random() * passwordLength);
+  }
+  // for (var i = 0; i < passwordLength; i++) {
+  //   userPassword.push(lowerCasedCharacters.splice(Math.random() * (lowerCasedCharacters.length - 1), 1).pop());
+  // }
+
+  // userPassword.concat(lowerCasedCharacters); // then access lowerCasedCharacters array with possibly with "Math.random().toString(36).slice(2, 10)" from StackOverflow (https://stackoverflow.com/questions/1497481/javascript-password-generator)
   // alert("Your password will feature lowercase letters.");
+
 } else if (confirm == false) {
   alert("Your password won't contain any lowercase letters.");
+  console.log(RandomLowerString);
 }
-console.log("Lowercase chosen.");
-;
+console.log("Lowercase chosen: " + passLowerCase);
 
-const passUpperCase = confirm("Should your password contain uppercase characters?");
+// Alert to choose Upper Case
+
+var passUpperCase = confirm("Should your password contain uppercase characters?");
 
 if (confirm == true) {
-  userPassword.push(upperCasedCharacters);// then access upperCasedCharacters array with possibly with "Math.random().toString(36).slice(2, 10)" from StackOverflow (https://stackoverflow.com/questions/1497481/javascript-password-generator)
+  // userPassword.concat(upperCasedCharacters);
+  var upperPassword = upperCasedCharacters[Math.floor(Math.random() * passwordLength)];
+  // then access upperCasedCharacters array with possibly with "Math.random().toString(36).slice(2, 10)" from StackOverflow (https://stackoverflow.com/questions/1497481/javascript-password-generator)
   // alert("Your password will feature uppercase letters.")
 } else if (confirm == false) {
   alert("Your password won't contain any uppercase letters.")
 }
-console.log("Uppercase chosen.");
-;
+console.log("Uppercase chosen: " + passUpperCase);
 
-function getPasswordOptions() {
-  var userPassword = prompt("")
+// Alert to choose numbers.
+
+var passNumerals = confirm("Should your password contain numbers?");
+
+if (confirm == true) {
+  var numberPassword = numericCharacters[Math.floor(Math.random() * passwordLength)];
+  // then access upperCasedCharacters array with possibly with "Math.random().toString(36).slice(2, 10)" from StackOverflow (https://stackoverflow.com/questions/1497481/javascript-password-generator)
+  // alert("Your password will feature uppercase letters.")
+} else if (confirm == false) {
+  alert("Your password won't contain any numbers.")
 }
+console.log("Numerals chosen?: " + passNumerals);
+
+var passSpecial = confirm("Should your password contain any special characters?");
+
+if (confirm == true) {
+  for (var i = 0; i < passwordLength.length; i++){
+  var specialPassword = specialCharacters[Math.floor(Math.random() * passwordLength)];}
+  // then access upperCasedCharacters array with possibly with "Math.random().toString(36).slice(2, 10)" from StackOverflow (https://stackoverflow.com/questions/1497481/javascript-password-generator)
+  // alert("Your password will feature uppercase letters.")
+} else if (confirm == false) {
+  alert("Your password won't contain any special characters.")
+  console.log(specialPassword);
+}
+console.log("Special characters chosen?: " + passSpecial);
+
+
+console.log(userPassword);
+
+// function getPasswordOptions() {
+//   var userPassword = prompt("")
+//   return passLowerCase + passUpperCase + passSpecialCase + passNumerals
+// }
+
+// function shuffle(userPassword) {
+//   let currentIndex = userPassword.length, randomIndex;
+
+//   // While there remain elements to shuffle.
+//   while (currentIndex != 0) {
+
+//     // Pick a remaining element.
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex--;
+
+//     // And swap it with the current element.
+//     [userPassword[currentIndex], userPassword[randomIndex]] = [
+//       userPassword[randomIndex], userPassword[currentIndex]];
+//   }
+
+//   return userPassword;
+// }
+// var arr = userPassword;
+// shuffle(arr);
+// console.log(arr);
+
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
